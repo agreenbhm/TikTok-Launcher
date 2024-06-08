39ca11c659b35cf65ffa84a1eab68d51cbaa49e6
Drew Green <agreenbhm@gmail.com>
2024-06-08 08:05:13 -0400

    Update adb_port.sh to enclose URL in quotes

--- adb_port.sh
+++ adb_port.sh
@@ -4,12 +4,12 @@ then
 fi
 PORT=$(cat /data/data/com.termux/files/home/adb_port.txt)
 echo $PORT
-echo $1
+echo "$1"
 /data/data/com.termux/files/usr/bin/adb connect localhost:$PORT | grep refused
 if [ $? == 1 ]
 then
-  /data/data/com.termux/files/usr/bin/adb shell am start --user 10 $1
+  /data/data/com.termux/files/usr/bin/adb shell am start --user 10 "$1"
   /data/data/com.termux/files/usr/bin/adb disconnect
 else
-  /data/data/com.termux/files/usr/bin/python3 /data/data/com.termux/files/home/launch_tiktok.py $1
+  /data/data/com.termux/files/usr/bin/python3 /data/data/com.termux/files/home/launch_tiktok.py "$1"
 fi
